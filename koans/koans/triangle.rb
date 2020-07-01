c29a1a04048432723e5fae5522f16019dd25ae41
alexander <agarber@profectusgroup.com>
2020-07-01 20:36:36 +1000

    triangles

--- koans/koans/triangle.rb
+++ koans/koans/triangle.rb
@@ -14,7 +14,27 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  check_third_side = lambda do |x, y|
+    if y > (x * 1.5)
+      raise TriangleError
+    else
+      :isosceles
+    end
+  end
+
+  raise TriangleError if (a <= 0) || (b <= 0) || (c <= 0)
+
+  if (a == b) && (a == c) && (b == c)
+    :equilateral
+  elsif a == b
+    check_third_side.call(b, c)
+  elsif a == c
+    check_third_side.call(c, b)
+  elsif b == c
+    check_third_side.call(c, a)
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
