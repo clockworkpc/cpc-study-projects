bc099f6d3c30df21efd0197a88d4bf7a7383fdbf
alexander <agarber@profectusgroup.com>
2020-07-08 23:24:26 +1000

    world

--- game-of-life/lib/game_of_life/world.rb
+++ game-of-life/lib/game_of_life/world.rb
@@ -1,60 +1,145 @@
 module GameOfLife
   class World
-    attr_reader :grid, :range
-
     def initialize(size)
       @size = size
+    end
+
+    def odd_value_range
+      calc = ->(a) { (@size / 2) * a }
+      first = calc.call(-1)
+      last = calc.call(1)
+      (first..last).to_a
+    end
+
+    def even_value_range
+      calc = ->(a, b) { ((@size / 2) + a) * b }
+      first = calc.call(-1, -1)
+      last = calc.call(0, 1)
+      (first..last).to_a
+    end
+
+    def range
+      @range ||= @size.odd? ? odd_value_range : even_value_range
     end
 
     def create_empty_grid
       grid = []
 
-      @size.times do |y|
-        @size.times do |x|
+      range.reverse_each do |y|
+        range.each do |x|
           grid << { x: x, y: y, life: false }
         end
       end
       @grid = grid
     end
 
+    def populate_grid(*coordinates)
+      coordinates.flatten(1).each do |xy|
+        x, y = xy
+        cell = find_cell(x, y)
+        cell[:life] = true
+      end
+    end
+
+    def create_grid
+      create_empty_grid
+      populate_grid(*coordinates)
+    end
+
     def grid
       @grid ||= create_grid
     end
 
-    def relative_position(cell, neighbour)
-      north_of = ->(a, b) { a[:y] == (b[:y] - 1) }
-      south_of = ->(a, b) { a[:y] == (b[:y] + 1) }
-      west_of = ->(a, b) { a[:x] == (b[:x] - 1) }
-      east_of = ->(a, b) { a[:x] == (b[:x] + 1) }
+    def relative_position_to_cell(cell, neighbour)
+      direction = ->(a, b, k, n) { a[k] == b[k] + n }
 
-      north = 'north' if north_of.call(cell, neighbour)
-      south = 'south' if south_of.call(cell, neighbour)
-      east = 'east' if east_of.call(cell, neighbour)
-      west = 'west' if west_of.call(cell, neighbour)
+      north = 'north' if direction.call(cell, neighbour, :y, 1)
+      south = 'south' if direction.call(cell, neighbour, :y, -1)
+      east = 'east' if direction.call(cell, neighbour, :x, 1)
+      west = 'west' if direction.call(cell, neighbour, :x, -1)
 
       [north, south, east, west].compact.join('_').to_sym
     end
 
-    def status(_x, _y)
-      :dead
-    end
-
-    def report
-      @grid.map do |hsh|
-        key = ['cell', hsh[:x], hsh[:y]].join('_').to_sym
-        status = status(hsh[:x], hsh[:y])
-        [key, status]
-      end.to_h
-    end
-
-    def cell(x, y)
+    def find_cell(x, y)
       @grid.find { |hsh| hsh[:x] == x && hsh[:y] == y }
     end
 
+    def neighbouring_coordinates(cell)
+      x, y = [cell[:x], cell[:y]]
+      {
+        north_west: [x - 1, y + 1],
+        north: [x, y + 1],
+        north_east: [x + 1, y + 1],
+        west: [x - 1, y],
+        east: [x + 1, y],
+        south_west: [x - 1, y - 1],
+        south: [x, y - 1],
+        south_east: [x + 1, y - 1]
+      }
+    end
+
+    def adjacent_cells(cell)
+      neighbouring_coordinates(cell)
+        .values.map { |v| find_cell(v[0], v[1]) }.compact
+    end
+
+    def neighbours(cell)
+      adjacent_cells(cell).select { |c| c[:life] }
+    end
+
+    def populated_status(cell)
+      count = neighbours(cell).count
+
+      lonely = :lonely if count < 2
+      happy = :happy if (2..3).include?(count)
+      crowded = :crowded if count >= 4
+
+      [lonely, happy, crowded].compact.join.to_sym
+    end
+
+    def empty_status(cell)
+      count = neighbours(cell).count
+      count == 3 ? :fertile : :dead
+    end
+
+    def current_status(x, y)
+      cell = find_cell(x, y)
+      status = cell[:life] ? populated_status(cell) : empty_status(cell)
+    end
+
+    def future_status(x, y)
+      :dead
+    end
+
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/MethodLength
+    def report_today
+      table = {}
+      key_index = 0
+      column_key = ->(i) { i >= @size ? i % @size : i }
+
+      grid.each_with_index do |cell, i|
+        key_index = (i / @size) if (i % @size).zero?
+        row = "row#{key_index}".to_sym
+        table[row] ||= {}
+        column = "col#{column_key.call(i)}".to_sym
+        table[row][column] = current_status(cell[:x], cell[:y])
+      end
+
+      table
+    end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Metrics/MethodLength
+
     def toggle(x, y)
-      cell = cell(x, y)
+      cell = find_cell(x, y)
       cell[:life] = cell[:life] != true
       cell
+    end
+
+    def draw_grid
+      # TODO
     end
   end
 end
--- game-of-life/spec/game_of_life/world_spec.rb
+++ game-of-life/spec/game_of_life/world_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe GameOfLife::World do
   let(:range_size_5) { [-2, -1, 0, 1, 2] }
   let(:range_size_6) { [-2, -1, 0, 1, 2, 3] }
 
-
-
   let(:empty_grid) do
     [
       { x: -1, y: 1, life: false }, { x: 0, y: 1, life: false }, { x: 1, y: 1, life: false },
@@ -17,16 +15,68 @@ RSpec.describe GameOfLife::World do
     ]
   end
 
+  let(:populated_grid) do
+    [
+      { x: -1, y: 1, life: false }, { x: 0, y: 1, life: true }, { x: 1, y: 1, life: true },
+      { x: -1, y: 0, life: false }, { x: 0, y: 0, life: true }, { x: 1, y: 0, life: true },
+      { x: -1, y: -1, life: false }, { x: 0, y: -1, life: true }, { x: 1, y: -1, life: false }
+    ]
+  end
+
   let(:empty_world_report) do
     {
-      cell_0_0: :dead, cell_1_0: :dead, cell_2_0: :dead,
-      cell_0_1: :dead, cell_1_1: :dead, cell_2_1: :dead,
-      cell_0_2: :dead, cell_1_2: :dead, cell_2_2: :dead
+      row0: { col0: :dead, col1: :dead, col2: :dead },
+      row1: { col0: :dead, col1: :dead, col2: :dead },
+      row2: { col0: :dead, col1: :dead, col2: :dead }
+    }
+  end
+
+  let(:first_generation_report) do
+    {
+      row0: { col0: :dead, col1: :happy, col2: :happy },
+      row1: { col0: :fertile, col1: :crowded, col2: :crowded },
+      row2: { col0: :dead, col1: :happy, col2: :fertile }
     }
   end
 
   let(:first_cell_original) { { x: 0, y: 0, life: false } }
   let(:first_cell_toggled) { { x: 0, y: 0, life: true } }
+
+  let(:cell_centre_neighbours) do
+    [
+      { x: -1, y: 1, life: false }, { x: 0, y: 1, life: false }, { x: 1, y: 1, life: false },
+      { x: -1, y: 0, life: false }, { x: 1, y: 0, life: false },
+      { x: -1, y: -1, life: false }, { x: 0, y: -1, life: false }, { x: 1, y: -1, life: false }
+    ]
+  end
+
+  let(:north_west_cell_neighbours) do
+    [
+      { x: 0, y: 1, life: false },
+      { x: -1, y: 0, life: false }, { x: 0, y: 0, life: false }
+    ]
+  end
+
+  let(:north_east_cell_neighbours) do
+    [
+      { x: 0, y: 1, life: false },
+      { x: 0, y: 0, life: false }, { x: 1, y: 0, life: false }
+    ]
+  end
+
+  let(:south_west_cell_neighbours) do
+    [
+      { x: -1, y: 0, life: false }, { x: 0, y: 0, life: false },
+      { x: 0, y: -1, life: false }
+    ]
+  end
+
+  let(:south_east_cell_neighbours) do
+    [
+      { x: 0, y: 0, life: false }, { x: 1, y: 0, life: false },
+      { x: 0, y: -1, life: false }
+    ]
+  end
 
   let(:toggled_grid) do
     [
@@ -36,80 +86,113 @@ RSpec.describe GameOfLife::World do
     ]
   end
 
+  describe 'New Worlds' do
+    it 'returns the correct range size' do
+      execute_range_test(3, range_size_3)
+      execute_range_test(4, range_size_4)
+      execute_range_test(5, range_size_5)
+      execute_range_test(6, range_size_6)
+    end
+
+    def execute_range_test(size, expectation)
+      puts "Range should be #{expectation}"
+      expect(described_class.new(size).range).to eq(expectation)
+    end
+  end
+
   describe 'New 3x3 World' do
     before(:all) do
       @subject = described_class.new(3)
     end
 
-    it 'range for 3 should (-1..1)' do
-      expect(@subject.range).to eq()
+    it 'generates an empty grid' do
+      expect(@subject.create_empty_grid).to eq(empty_grid)
     end
 
-    # it 'generates an empty grid' do
-    #   expect(@subject.create_empty_grid).to eq(empty_grid)
-    # end
+    it 'returns an overview of the grid' do
+      expect(@subject.report_today).to eq(empty_world_report)
+    end
 
-    # it 'returns an overview of the grid' do
-    #   expect(@subject.report).to eq(empty_world_report)
-    # end
+    it 'retrieves the details of a cell' do
+      expect(@subject.find_cell(0, 0)).to eq(first_cell_original)
+    end
 
-    # it 'retrieves the details of a cell' do
-    #   expect(@subject.cell(0, 0)).to eq(first_cell_original)
-    # end
+    it 'returns nil for out-of-bounds cell coordinates' do
+      expect(@subject.find_cell(100, 100)).to eq(nil)
+    end
 
-    # it 'returns that [0, 0] is NORTH [0, 1]' do
-    #   cell = @subject.cell(0, 0)
-    #   neighbour = @subject.cell(0, 1)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:north)
-    # end
+    it 'returns the coordinates of the neighbour' do
+      execute_relative_position_test([0, 0], [0, 1], :south)
+      execute_relative_position_test([0, 1], [0, 0], :north)
+      execute_relative_position_test([0, 0], [1, 0], :west)
+      execute_relative_position_test([1, 0], [0, 0], :east)
+      execute_relative_position_test([0, 0], [1, 1], :south_west)
+      execute_relative_position_test([1, 1], [0, 0], :north_east)
+      execute_relative_position_test([0, 1], [1, 0], :north_west)
+      execute_relative_position_test([1, 0], [0, 1], :south_east)
+    end
 
-    # it 'returns that [0, 1] is SOUTH [0, 0]' do
-    #   cell = @subject.cell(0, 1)
-    #   neighbour = @subject.cell(0, 0)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:south)
-    # end
+    def execute_relative_position_test(cell_xy, neighbour_xy, expectation)
+      puts "Relation of #{cell_xy} to #{neighbour_xy} is #{expectation}"
+      cx, cy = cell_xy
+      nx, ny = neighbour_xy
+      cell = @subject.find_cell(cx, cy)
+      neighbour = @subject.find_cell(nx, ny)
+      expect(@subject.relative_position_to_cell(cell, neighbour)).to eq(expectation)
+    end
 
-    # it 'returns that [0, 0] is WEST [1, 0]' do
-    #   cell = @subject.cell(0, 0)
-    #   neighbour = @subject.cell(1, 0)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:west)
-    # end
+    it 'toggles a cell' do
+      expect(@subject.toggle(0, 0)).to eq(first_cell_toggled)
+      expect(@subject.toggle(0, 0)).to eq(first_cell_original)
+      expect(@subject.toggle(0, 0)).to eq(first_cell_toggled)
+      expect(@subject.toggle(0, 0)).to eq(first_cell_original)
+    end
 
-    # it 'returns that [1, 0] is EAST [0, 0]' do
-    #   cell = @subject.cell(1, 0)
-    #   neighbour = @subject.cell(0, 0)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:east)
-    # end
+    it 'returns current_status' do
+      expect(@subject.current_status(0, 0)).to eq(:dead)
+    end
 
-    # it 'returns that [0, 0] is NORTHWEST [1, 1]' do
-    #   cell = @subject.cell(0, 0)
-    #   neighbour = @subject.cell(1, 1)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:north_west)
-    # end
+    it 'returns neighbours of south_east cell' do
+      cell = @subject.find_cell(1, -1)
+      expect(@subject.adjacent_cells(cell)).to eq(south_east_cell_neighbours)
+    end
 
-    # it 'returns that [1, 1] is SOUTHEAST [0,0]' do
-    #   cell = @subject.cell(1, 1)
-    #   neighbour = @subject.cell(0, 0)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:south_east)
-    # end
+    it 'returns the neighbours of a given cell' do
+      execute_neighbouring_cells_test([0, 0], cell_centre_neighbours)
+      execute_neighbouring_cells_test([-1, 1], north_west_cell_neighbours)
+      execute_neighbouring_cells_test([1, 1], north_east_cell_neighbours)
+      execute_neighbouring_cells_test([-1, -1], south_west_cell_neighbours)
+      execute_neighbouring_cells_test([1, -1], south_east_cell_neighbours)
+    end
 
-    # it 'returns that [0,1] is SOUTHWEST [1,0]' do
-    #   cell = @subject.cell(0, 1)
-    #   neighbour = @subject.cell(1, 0)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:south_west)
-    # end
+    def execute_neighbouring_cells_test(xy_ary, expectation)
+      x, y = xy_ary
+      neighbour_coordinates = expectation.map { |hsh| [hsh[:x], hsh[:y]] }
+      puts "Neighbouring cells of #{xy_ary} should be #{neighbour_coordinates}"
+      cell = @subject.find_cell(x, y)
+      expect(@subject.adjacent_cells(cell)).to eq(expectation)
+    end
+  end
 
-    # it 'returns that [1,0] is NORTHEAST [0,1]' do
-    #   cell = @subject.cell(1, 0)
-    #   neighbour = @subject.cell(0, 1)
-    #   expect(@subject.relative_position(cell, neighbour)).to eq(:north_east)
-    # end
+  describe 'Populated 3x3 World' do
+    before(:all) do
+      @subject = described_class.new(3)
+      @subject.create_empty_grid
+    end
 
-    # it 'toggles a cell' do
-    #   expect(@subject.toggle(0, 0)).to eq(first_cell_toggled)
-    #   expect(@subject.toggle(0, 0)).to eq(first_cell_original)
-    #   expect(@subject.toggle(0, 0)).to eq(first_cell_toggled)
-    #   expect(@subject.toggle(0, 0)).to eq(first_cell_original)
-    # end
+    it 'starts with an empty grid' do
+      expect(@subject.grid).to eq(empty_grid)
+    end
+
+    it 'vivifies [0, -1], [0,0], [0,1], [1,1], [1,0]' do
+      coordinates = [[0, - 1], [0, 0], [0, 1], [1, 1], [1, 0]]
+      @subject.populate_grid(coordinates)
+      expect(@subject.grid).to eq(populated_grid)
+    end
+
+    it 'reports on the current status of the cells' do
+      report = @subject.report_today
+      expect(report).to eq(first_generation_report)
+    end
   end
 end
