304969b7f7815810125bfb08a6ee684724f62cb1
alexander <agarber@profectusgroup.com>
2020-07-10 22:29:45 +1000

    game complete

--- game-of-life/lib/game_of_life/engine.rb
+++ game-of-life/lib/game_of_life/engine.rb
@@ -5,32 +5,35 @@ module GameOfLife
     attr_reader :world
 
     def create_world
-      # size = (3..22).to_a.sample
-      # puts 'How many cells long and wide should the world be? '
-      # p '> '
-      # size = gets.chomp.to_i
       size = 22
       @world = World.new(size)
     end
 
     def seed_world
       n = (@world.size..(@world.size**2)).to_a.sample
-      # puts 'How many cells do you want to seed? '
-      # p '> '
-      # n = gets.chomp.to_i
       @world.seed_grid(n)
     end
 
+    def display_statistics
+      puts Rainbow("Day: #{@counter}").orange
+      puts Rainbow("Population: #{@world.population}").orange
+
+      diff = @population.last - @population[-2].to_i
+      diff_colour = diff < 0 ? :red : :green
+      puts Rainbow("Difference: #{diff}").send(diff_colour)
+      puts @world.draw_grid.gsub('x', Rainbow('x').green)
+    end
+
     def start
-      counter = 0
+      @counter = 0
       running = true
+      @population = []
       while running
         system('clear')
-        counter += 1
-        puts Rainbow("Day: #{counter}").orange
-        puts Rainbow("Population: #{@world.population}").orange
-        puts "\n"
-        puts @world.draw_grid.gsub('x', Rainbow('x').green)
+        @counter += 1
+        @population << @world.population
+        display_statistics
+
         @world.toggle_grid
         sleep 0.5
       end
@@ -43,7 +46,3 @@ module GameOfLife
     end
   end
 end
-
-# puts 'starting game'
-# g = GameOfLife::Engine.new
-# g.play
