910fc0f3b37c3bff4a34e6389ff5409f0187e179
alexander <clockworkpc@gmail.com>
2021-01-22 11:54:57 +1100

    completed day 3

--- advent_of_code_2020/aoc2020ruby/lib/advent_of_code/toboggan_trajectory.rb
+++ advent_of_code_2020/aoc2020ruby/lib/advent_of_code/toboggan_trajectory.rb
@@ -1,5 +1,54 @@
 module AdventOfCode
   class TobogganTrajectory
-    def itinerary(down: nil, right: nil, left: nil); end
+    def itinerary(moves:, down:, right:)
+      x, y = [0, 0]
+      itin = [{ x: x, y: y }]
+
+      moves.times do
+        x += right
+        y += down
+        itin << { x: x, y: y }
+      end
+      
+      itin
+    end
+
+    def plotted_cells(lines:, moves:, down:, right:)
+      itin = itinerary(moves: moves, down: down, right: right)
+      width = lines.first.strip.length
+
+      cell = lambda do |hsh|
+        row = lines[hsh[:y]]
+        first_leg = hsh[:x] <= width - 1
+        cell_index = first_leg ? hsh[:x] : hsh[:x] % width
+        row[cell_index]
+      end
+
+      itin.each_with_object([]) do |hsh, ary|
+        ary << cell.call(hsh)
+      end
+    end
+
+    def journey_cells(lines:, down:, right:)
+      quotient = (lines.count.to_f / down).to_i
+      moves = down > 1 ? quotient : quotient - 1
+
+      cells = plotted_cells(lines: lines, moves: moves, down: down, right: right)
+      trees = cells.count { |cell| cell.eql?('#') }
+
+      {
+        cells: cells,
+        trees: trees
+      }
+    end
+
+    def tree_count_product(lines:, journeys:)
+      trees_ary = journeys.each_with_object([]) do |journey, ary|
+        jc = journey_cells(lines: lines, down: journey[:down], right: journey[:right])
+        ary << jc[:trees]
+      end
+
+      trees_ary.inject(&:*)
+    end
   end
 end
--- advent_of_code_2020/aoc2020ruby/spec/advent_of_code/toboggan_trajectory_spec.rb
+++ advent_of_code_2020/aoc2020ruby/spec/advent_of_code/toboggan_trajectory_spec.rb
@@ -1,25 +1,123 @@
 require './lib/advent_of_code/toboggan_trajectory'
 
 RSpec.describe AdventOfCode::TobogganTrajectory do
-  let(:sample_input) { File.readlines('./spec/fixtures/input_day_03.txt') }
+  let(:puzzle_input) { File.readlines('./spec/fixtures/input_day_03.txt') }
 
-  let(:sample_itinerary) do
+  let(:sample_input) do
+    txt = <<~HEREDOC
+      ..##.......
+      #...#...#..
+      .#....#..#.
+      ..#.#...#.#
+      .#...##..#.
+      ..#.##.....
+      .#.#.#....#
+      .#........#
+      #.##...#...
+      #...##....#
+      .#..#...#.#
+    HEREDOC
+
+    txt.split("\n")
+  end
+
+  let(:sample_plotted_cells) do
     [
-      { x: 1, y: 1 },
-      { x: 4, y: 2 },
-      { x: 7, y: 3 },
-      { x: 10, y: 4 },
-      { x: 13, y: 5 },
-      { x: 16, y: 6 },
-      { x: 19, y: 7 },
-      { x: 22, y: 8 },
-      { x: 25, y: 9 },
-      { x: 28, y: 10 },
-      { x: 31, y: 11 }
+      sample_input[0][0],
+      sample_input[1][3],
+      sample_input[2][6],
+      sample_input[3][9],
+      sample_input[4][1],
+      sample_input[5][4],
+      sample_input[6][7],
+      sample_input[7][10],
+      sample_input[8][2],
+      sample_input[9][5],
+      sample_input[10][8]
     ]
   end
 
-  it 'returns coordinates for journey' do
-    expect(subject.itinerary(down: 1, right: 3)).to eq(sample_itinerary)
+  let(:sample_itinerary) do
+    [
+      { x: 0, y: 0 },
+      { x: 3, y: 1 },
+      { x: 6, y: 2 },
+      { x: 9, y: 3 },
+      { x: 12, y: 4 },
+      { x: 15, y: 5 },
+      { x: 18, y: 6 },
+      { x: 21, y: 7 },
+      { x: 24, y: 8 },
+      { x: 27, y: 9 },
+      { x: 30, y: 10 }
+    ]
+  end
+
+  let(:sample_journeys) do
+    [
+      { right: 1, down: 1 },
+      { right: 3, down: 1 },
+      { right: 5, down: 1 },
+      { right: 7, down: 1 },
+      { right: 1, down: 2 }
+    ]
+  end
+
+  describe 'Part 1' do
+    it 'returns coordinates for journey' do
+      expect(subject.itinerary(moves: 10, down: 1, right: 3)).to eq(sample_itinerary)
+    end
+
+    it 'returns cell value for each row' do
+      expect(subject.plotted_cells(lines: sample_input, moves: 10, down: 1, right: 3))
+        .to eq(sample_plotted_cells)
+    end
+
+    it 'returns cell value for each row of full journey' do
+      expect(subject.journey_cells(lines: sample_input, down: 1, right: 3))
+        .to eq({ cells: sample_plotted_cells, trees: 7 })
+    end
+
+    it 'returns tree count for puzzle input' do
+      journey = subject.journey_cells(lines: puzzle_input, down: 1, right: 3)
+      expect(journey[:trees]).to eq(223)
+    end
+  end
+
+  describe 'Part 2' do
+    it 'returns 2 trees for Right 1, Down 1' do
+      journey = subject.journey_cells(lines: sample_input, down: 1, right: 1)
+      expect(journey[:trees]).to eq(2)
+    end
+
+    it 'returns 7 trees for Right 3, Down 1' do
+      journey = subject.journey_cells(lines: sample_input, down: 1, right: 3)
+      expect(journey[:trees]).to eq(7)
+    end
+
+    it 'returns 3 trees for Right 5, Down 1' do
+      journey = subject.journey_cells(lines: sample_input, down: 1, right: 5)
+      expect(journey[:trees]).to eq(3)
+    end
+
+    it 'returns 4 trees for Right 7, Down 1' do
+      journey = subject.journey_cells(lines: sample_input, down: 1, right: 7)
+      expect(journey[:trees]).to eq(4)
+    end
+
+    it 'returns 2 trees for Right 1, Down 2' do
+      journey = subject.journey_cells(lines: sample_input, down: 2, right: 1)
+      expect(journey[:trees]).to eq(2)
+    end
+
+    it 'returns tree-count product of 336 for sample input' do
+      tcp = subject.tree_count_product(lines: sample_input, journeys: sample_journeys)
+      expect(tcp).to eq(336)
+    end
+
+    it 'returns tree-count product of 3_517_401_300 for puzzle input' do
+      tcp = subject.tree_count_product(lines: puzzle_input, journeys: sample_journeys)
+      expect(tcp).to eq(3_517_401_300)
+    end
   end
 end
