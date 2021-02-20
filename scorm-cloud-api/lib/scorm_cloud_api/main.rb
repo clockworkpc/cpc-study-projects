1f75b73177c6a1cd53e8d0fafc54c7effb5008c6
alexander <clockworkpc@gmail.com>
2021-02-20 20:40:33 +1100

    scorm cloud

--- scorm-cloud-api/lib/scorm_cloud_api/main.rb
+++ scorm-cloud-api/lib/scorm_cloud_api/main.rb
@@ -1,39 +1,38 @@
 require 'rustici_software_cloud_v2'
-
 module ScormCloudApi
   class Main
     def initialize
       RusticiSoftwareCloudV2.configure do |config|
         config.username = ENV['SCORM_CLOUD_APP_ID']
         config.password = ENV['SCORM_CLOUD_SECRET_KEY']
-
-        # Configure OAuth2 access token for authorization: OAUTH
         # config.access_token = 'YOUR ACCESS TOKEN'
       end
-
     end
 
-    def api_instance
-      RusticiSoftwareCloudV2::AboutApi.new
+    def about_api_instance
+      @about_api_instance ||= RusticiSoftwareCloudV2::AboutApi.new
+    end
+
+    def course_api_instance
+      @course_api_instance ||= RusticiSoftwareCloudV2::CourseApi.new
     end
 
     def about
-      begin
-        result = api_instance.get_about
-        p result
-      rescue RusticiSoftwareCloudV2::ApiError => e
-        puts "Exception when calling AboutApi->get_about: #{e}"
-      end
+      about_api_instance.get_about
+    rescue RusticiSoftwareCloudV2::ApiError => e
+      puts "Exception when calling AboutApi->get_about: #{e}"
     end
 
-
-
-    def hello
-      :hello
+    def courses
+      course_api_instance.get_courses
     end
 
+    def course(id:)
+      course_api_instance.get_course(id)
+    end
+
+    def course_configuration(id:)
+      course_api_instance.get_course_configuration(id)
+    end
   end
 end
-
-# Setup authorization
-
--- scorm-cloud-api/spec/scorm_cloud_api/main_spec.rb
+++ scorm-cloud-api/spec/scorm_cloud_api/main_spec.rb
@@ -2,12 +2,34 @@ require 'spec_helper'
 require './lib/scorm_cloud_api/main'
 
 RSpec.describe ScormCloudApi::Main do
-  it 'hello' do
-    expect(subject.hello).to eq(:hello)
+  let(:course_config_json) do
+    path = './spec/fixtures/course_configuration.json'
+    JSON.parse(File.read(path), { symbolize_names: true })
   end
 
   it 'returns API version' do
     res = subject.about
-    pp res
+    expect(res.distribution).to eq('SCORM Cloud')
+    expect(res.version).to eq('Api V2')
+  end
+
+  it 'returns all courses' do
+    res = subject.courses
+    ref_test = res.courses.first
+    expect(ref_test.activity_id).to eq(ENV['SCORM_CLOUD_USERNAME'])
+    expect(ref_test.course_learning_standard).to eq('SCORM12')
+    expect(ref_test.id).to eq('reference_test')
+  end
+
+  it 'returns reference_test course details' do
+    res = subject.course(id: 'reference_test')
+    expect(res.activity_id).to eq(ENV['SCORM_CLOUD_USERNAME'])
+    expect(res.course_learning_standard).to eq('SCORM12')
+    expect(res.id).to eq('reference_test')
+  end
+
+  it 'returns reference_test course configuration' do
+    res = subject.course_configuration(id: 'reference_test')
+    expect(res.to_hash).to eq(course_config_json)
   end
 end
